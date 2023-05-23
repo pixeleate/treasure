235879cec15c061ad6516b955d926f3fe33792e1
Ulises Garcia <dev@pixeleate.com>
2023-05-23 15:56:58 -0500

    Removes TheGraph client and graphql-request

--- lib/client.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-import { getSdk } from "../generated/app.generated";
-
-const client = getSdk(new GraphQLClient(process.env.CRYPTOPUNKS_API_URL!));
-
-export default client;
